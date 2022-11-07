5de41925d8b5f80a872b5ceece78783078477b83
Igor <79312270368@yandex.ru>
2022-11-07 21:51:50 +0300

    Initial Commit

---
+++ VCClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VCClient
+//
+//  Created by Игорь Павлов on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
